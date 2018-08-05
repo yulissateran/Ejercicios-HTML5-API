4c3c7df3aab8f777528868a92ab56a1897e1077e
yulissateran <yulissa.lteran@gmail.com>
2018-08-05 01:25:36 -0500

    video y foto instantanea con html5

---
+++ video-html5/app.js
@@ -0,0 +1,9 @@
+navigator.mediaDevices.getUserMedia({ audio: false, video: true}).then((stream)=>{
+
+  console.log(stream);
+  let video = document.getElementById('video');
+  video.srcObject = stream
+  video.onloadedmetadata =(ev)=> video.play()
+    }).catch((error)=>{
+      console.log(error);
+    });
